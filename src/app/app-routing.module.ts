ebf142d2864bdab9be2cb0daa6621c945af0328f
Juan Munoz <juano.mube@gmail.com>
2023-05-02 22:07:09 +0200

    progress in home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import { ResumeComponent } from './resume/resume.component';
+import { PlaygroundComponent } from './playground/playground.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '', pathMatch: 'full', component: HomeComponent },
+    { path: 'resume', component: ResumeComponent },
+    { path: 'playground', component: PlaygroundComponent },
+    { path: 'contact', component: ContactComponent },
     { path: '**', component: HomeComponent}
   ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HeaderComponent } from './header/header.component';
 import { MainSectionComponent } from './main-section/main-section.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PlaygroundComponent } from './playground/playground.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     MainSectionComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ResumeComponent,
+    PlaygroundComponent,
+    ContactComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -30,8 +39,11 @@ import { HomeComponent } from './home/home.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    MatSidenavModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatMenuModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
